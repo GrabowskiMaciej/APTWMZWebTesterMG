6ed4da6411caf770cd5e19f558359d7d4114894a
GrabowskiMaciej <grabow.maciej@gmail.com>
2019-05-12 21:46:20 +0200

    Base Commit
    Commit of the program

---
+++ BaseWebTester/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Program
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCase2Test()
+        {
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2010");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Brak kwalifikacji", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2009");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2009");
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2008");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2008");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Skrzat", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2007");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2007");
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2006");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2006");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Mlodzik", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2005");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2005");
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2002");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2002");
+            driver.FindElement(By.Id("rodzice")).Click();
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Junior", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-2001");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Dorosly", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-1955");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Dorosly", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-1954");
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Blad danych", CloseAlertAndGetItsText());
+            driver.Navigate().GoToUrl("https://lamp.ii.us.edu.pl/~mtdyd/zawody/?");
+            driver.FindElement(By.Id("inputEmail3")).Click();
+            driver.FindElement(By.Id("inputEmail3")).Clear();
+            driver.FindElement(By.Id("inputEmail3")).SendKeys("Jan");
+            driver.FindElement(By.Id("inputPassword3")).Clear();
+            driver.FindElement(By.Id("inputPassword3")).SendKeys("Kowalski");
+            driver.FindElement(By.Id("dataU")).Clear();
+            driver.FindElement(By.Id("dataU")).SendKeys("01-01-1954");
+            driver.FindElement(By.Id("lekarz")).Click();
+            driver.FindElement(By.XPath("//button[@type='button']")).Click();
+            Assert.AreEqual("Senior", CloseAlertAndGetItsText());
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
